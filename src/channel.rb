eb95314a890b50cb968ff213bc519f1bcd9f1462
sim-mokomo <15795655+sim-mokomo@users.noreply.github.com>
2021-05-29 15:55:35 +0900

    impl: channel class

---
+++ src/channel.rb
@@ -0,0 +1,39 @@
+class Channel
+  def initialize(channel_id)
+    @channel_id = channel_id
+    @youtube_service = YoutubeService.new
+  end
+
+  def search_videos
+    response = list_searches(nil)
+    videos = create_videos_from_response(response)
+    loop do
+      response = list_searches(response.next_page_token)
+      videos.concat(create_videos_from_response(response))
+      break if response.next_page_token.nil?
+    end
+
+    videos
+  end
+
+  private
+
+  def create_videos_from_response(response)
+    response.items.map do |x|
+      Video.new(
+        x.id.video_id,
+        x.snippet.title
+      )
+    end
+  end
+
+  # @return [Array<Google::Apis::YoutubeV3::SearchListsResponse>]
+  def list_searches(page_token)
+    @youtube_service.service.list_searches(
+      'id,snippet',
+      channel_id: @channel_id,
+      max_results: 50,
+      page_token: page_token
+    )
+  end
+end
