5834cfa986d9b5ff845e55d77094c8b9b2a60be2
sim-mokomo <15795655+sim-mokomo@users.noreply.github.com>
2021-05-30 16:48:57 +0900

    impl: class to hash

--- src/extensions/object_extensions.rb
+++ src/extensions/object_extensions.rb
@@ -1,1 +1,13 @@
+require 'json'
 
+class Object
+  def to_hash_object
+    hash = {}
+    instance_variables.each do |variable|
+      key_name = variable.to_s.delete('@')
+      value = instance_variable_get(variable)
+      hash[key_name] = value.instance_variables.length.positive? ? value.to_hash_object : value
+    end
+    hash
+  end
+end
