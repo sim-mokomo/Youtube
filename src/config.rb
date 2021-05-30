28dc25c9e1dc4557801ce5ab8f980631d1dac487
sim-mokomo <15795655+sim-mokomo@users.noreply.github.com>
2021-05-30 16:49:25 +0900

    impl: gas endpoint を指定できるように

--- src/config.rb
+++ src/config.rb
@@ -1,12 +1,13 @@
 require 'json'
 
 class Config
-  attr_reader :youtube_api_keys
+  attr_reader :youtube_api_keys, :gas_api_endpoint
 
   def initialize
     File.open('config.json') do |file|
       json = JSON.parse(file.read)
       @youtube_api_keys = json['youtube-api-keys']
+      @gas_api_endpoint = json['gas-api-endpoint']
     end
   end
 end
