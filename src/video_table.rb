f8b1460fa2d7ad102363f3e2b7f48bbb304601d9
sim-mokomo <15795655+sim-mokomo@users.noreply.github.com>
2021-05-30 16:37:37 +0900

    fix: sheet を table にリネーム

---
+++ src/video_table.rb
@@ -0,0 +1,63 @@
+require 'csv'
+
+class VideoTable
+  attr_reader :records
+
+  # @param [String] file_name
+  def initialize(base_dir, file_name)
+    @base_dir = base_dir
+    @file_name = file_name
+    @records = load
+  end
+
+  def file_path
+    File.expand_path("#{@base_dir}/#{@file_name}.json")
+  end
+
+  # @return [Array<VideoRecord>]
+  def load
+    return [] unless File.exist?(file_path)
+
+    @records.clear
+    File.open(file_path) do |file|
+      objs = JSON.parse(file.read)
+      return [] if objs.length.zero?
+
+      return objs.map { |x| VideoRecord.new(x['video_id'], x['title']) }
+    end
+  end
+
+  # @param record [VideoRecord]
+  def add_record(record)
+    @records.push(record)
+  end
+
+  def exist_record(record)
+    !@records.find { |x| x.video_id == record.video_id }.nil?
+  end
+
+  def overwrite_save(records)
+    @records = records
+    save
+  end
+
+  def save
+    hashes = @records.map(&:to_hash_object)
+    File.open(file_path, 'w') do |f|
+      f.puts(JSON.pretty_generate(hashes))
+    end
+  end
+
+  def empty
+    @records.length.zero?
+  end
+end
+
+class VideoRecord
+  attr_reader :video_id, :title
+
+  def initialize(video_id, title)
+    @video_id = video_id
+    @title = title
+  end
+end
