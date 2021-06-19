529e33c2e23d8a0e0dc7fb500acb1bf9902772e2
sim-mokomo <15795655+sim-mokomo@users.noreply.github.com>
2021-06-19 14:54:31 +0900

    move file

--- src/output_config.rb
+++ src/output_config.rb
@@ -4,7 +4,7 @@ class OutputConfig
   attr_reader :playlist_configs
 
   def initialize
-    File.open('./outputs_config/config.json') do |file|
+    File.open('./output_config.json') do |file|
       json_obj = JSON.parse(file.read)
       @playlist_configs = json_obj['playlist_configs'].map { |x| PlayListConfig.new(x['name'], x['id']) }
     end
